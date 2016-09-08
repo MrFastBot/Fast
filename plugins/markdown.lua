e80763bea0109a8ffc45408994a84b92a13a7a13
MrFastBot <MrFastBot@gmail.com>
2016-09-08 21:31:54 +0430

    Update markdown.lua

--- plugins/markdown.lua
+++ plugins/markdown.lua
@@ -1,10 +1,10 @@
 local function run(msg,matches)
 local text = matches[1]
-   send_api_msg(msg, get_receiver_api(msg), text, true, 'md')
+   send_api_msg(msg, get_receiver_api(msg), "*"text"*", true, 'md')
    end
 return {
 patterns = {
-"^[/!#]markdown (.*)$"
+"^[/!#]bold (.*)$"
 },
 run = run 
 }
